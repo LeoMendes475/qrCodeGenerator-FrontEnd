44bae6963510dd0cfbc572e3365e7cc09803e369
Leonardo Mendes <mendesleonardo475@gmail.com>
2023-03-28 23:46:58 -0300

    ajuste na conexão com backend

--- src/Service/index.js
+++ src/Service/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://vast-red-pig-yoke.cyclic.app/'
+    baseURL: 'https://vast-red-pig-yoke.cyclic.app'
 });
 
 export default api;
